7cbc723329449980bcb7526556269da1b885b458
Dhivya Janakiraman <dhivya.janakiraman91@gmail.com>
2016-04-09 00:58:25 -0700

    Adding modified server.js and the html pages

--- server.js
+++ server.js
@@ -1,116 +1,42 @@
-/*
- * GET customers listing.
- */
-exports.list = function(req, res){
-  req.getConnection(function(err,connection){
-       
-     connection.query('SELECT * FROM customer',function(err,rows)     {
-            
-        if(err)
-           console.log("Error Selecting : %s ",err );
-     
-            res.render('customers',{page_title:"Customers - Node.js",data:rows});
-                           
-         });
-       
-    });
-  
-};
-exports.add = function(req, res){
-  res.render('add_customer',{page_title:"Add Customers-Node.js"});
-};
-exports.edit = function(req, res){
-    
-  var id = req.params.id;
-    
-  req.getConnection(function(err,connection){
-       
-     connection.query('SELECT * FROM customer WHERE id = ?',[id],function(err,rows)
-        {
-            
-            if(err)
-                console.log("Error Selecting : %s ",err );
-     
-            res.render('edit_customer',{page_title:"Edit Customers - Node.js",data:rows});
-                           
-         });
-                 
-    }); 
-};
-/*Save the customer*/
-exports.save = function(req,res){
-    
-    var input = JSON.parse(JSON.stringify(req.body));
-    
-    req.getConnection(function (err, connection) {
-        
-        var data = {
-            
-            name    : input.name,
-            address : input.address,
-            email   : input.email,
-            phone   : input.phone 
-        
-        };
-        
-        var query = connection.query("INSERT INTO customer set ? ",data, function(err, rows)
-        {
-  
-          if (err)
-              console.log("Error inserting : %s ",err );
-         
-          res.redirect('/customers');
-          
-        });
-        
-       // console.log(query.sql); get raw query
-    
-    });
-};/*Save edited customer*/
-exports.save_edit = function(req,res){
-    
-    var input = JSON.parse(JSON.stringify(req.body));
-    var id = req.params.id;
-    
-    req.getConnection(function (err, connection) {
-        
-        var data = {
-            
-            name    : input.name,
-            address : input.address,
-            email   : input.email,
-            phone   : input.phone 
-        
-        };
-        
-        connection.query("UPDATE customer set ? WHERE id = ? ",[data,id], function(err, rows)
-        {
-  
-          if (err)
-              console.log("Error Updating : %s ",err );
-         
-          res.redirect('/customers');
-          
-        });
-    
-    });
-};
+// server.js
 
-exports.delete_customer = function(req,res){
-          
-     var id = req.params.id;
-    
-     req.getConnection(function (err, connection) {
-        
-        connection.query("DELETE FROM customer  WHERE id = ? ",[id], function(err, rows)
-        {
-            
-             if(err)
-                 console.log("Error deleting : %s ",err );
-            
-             res.redirect('/customers');
-             
-        });
-        
-     });
-};
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+//var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+//var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+//mongoose.connect(configDB.url); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
